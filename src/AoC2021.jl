9161eb117ce87128172f35501a1fb8900fb0f7e0
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-12-01 09:26:05 +0100

    Solution Day 1

--- src/AoC2021.jl
+++ src/AoC2021.jl
@@ -2,4 +2,6 @@ module AoC2021
 
 # Write your package code here.
 
+include("day01.jl")
+
 end
---
+++ src/day01.jl
@@ -0,0 +1,36 @@
+module Day01
+export day01
+
+function readInput(inputfile="inputs/input01.txt")
+    lines = parse.(Int, eachline(inputfile))
+    return lines
+end
+
+function day01(inputfile="inputs/input01.txt")
+    values = readInput(inputfile)
+    return (day01Part1(values), day01Part2(values))
+end
+
+function day01Part1(values)
+    #count(isless.(values[1:end-1], values[2:end]))
+    #@views mapreduce(isless, +, values[1:end-1], values[2:end])
+    return lagged_compare(values, 1)
+end
+
+function day01Part2(values)
+    #count(isless.(values[1:end-3], values[4:end]))
+    #@views mapreduce(isless, +, values[1:end-3], values[4:end])
+    return lagged_compare(values, 3)
+end
+
+function lagged_compare(values, lag=1)
+    count = 0
+    for i in 1:length(values)-lag
+        if values[i+lag] > values[i]
+            count += 1
+        end
+    end
+    return count
+end
+
+end #module
