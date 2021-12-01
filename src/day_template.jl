a845cd983bbeaf01c3564bfe7f415f2506194a95
Adrian Braemer <11058200+abraemer@users.noreply.github.com>
2021-12-01 09:26:27 +0100

    minor change to template

--- src/day_template.jl
+++ src/day_template.jl
@@ -7,21 +7,21 @@ end
 
 function readInput(inputfile="inputs/inputNNN.txt")
     lines = readlines(inputfile)
-    lines
+    return lines
     # [parseLine(line) for line in eachline(inputfile)]
 end
 
 function dayNNN(inputfile="inputs/inputNNN.txt")
     values = readInput(inputfile)
-    (dayNNNPart1(values), dayNNNPart2(values))
+    return (dayNNNPart1(values), dayNNNPart2(values))
 end
 
 function dayNNNPart1(values)
-    
+
 end
 
 function dayNNNPart2(values)
-    
+
 end
 
 end #module
